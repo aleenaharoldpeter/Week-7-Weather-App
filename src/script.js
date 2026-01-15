56a7410a767407d2b82f42bffa900b81c52a8316
Aleena Harold Peter <aleenaharoldpeter120@gmail.com>
2026-01-15 22:31:10 +0530

    Integrated API for temperature based on the city entered

--- src/script.js
+++ src/script.js
@@ -1,8 +1,21 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+}
+
+function searchCity(city) {
+    let apiKey = "5037o6a07f1bdbdcf547b34731aft69a";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 
 }
 
