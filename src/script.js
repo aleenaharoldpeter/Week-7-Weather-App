d6c57e9494f5912828a8aa0eb0a1d2c06f15ebc7
Aleena Harold Peter <aleenaharoldpeter120@gmail.com>
2026-01-15 23:23:56 +0530

    Incorporated and Styled Weather Icon via API

--- src/script.js
+++ src/script.js
@@ -6,6 +6,8 @@ function updateWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" alt="Broken Clouds"     class="weather-app-icon"/>`
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -21,10 +23,10 @@ function formatDate(date) {
     let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let day = days[date.getDay()];
     if(minutes < 10) {
-        minutes = `$0{minutes}`;
+        minutes = `0${minutes}`;
     }
     if(hours < 10) {
-        hours = `$0{hours}`;
+        hours = `0${hours}`;
     }
     return `${day} ${hours}:${minutes}`;
 }
